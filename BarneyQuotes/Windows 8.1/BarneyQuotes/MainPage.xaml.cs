ac6bc88c572f21a782c5c9f075a62cbf6b212428
rayd25051992 <rayd_250592@outlook.com>
2014-02-11 21:16:11 +0000

    Full dev process. Win 8.1 process

---
+++ BarneyQuotes/Windows 8.1/BarneyQuotes/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BarneyQuotes
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] responses = { "Think of me like Yoda, but instead of being little and green I wear suits and I’m awesome. I’m your bro—I’m Broda!", 
+                                     "It’s gonna be legend-… wait for it… and I hope you’re not lactose intolerant because the second half of that word is DAIRY!", 
+                                     "Here’s the mini-cherry on top of the regular cherry on top of the sundae of awesomeness that is my life", 
+                                     "Suits are full of joy. They’re the sartorial equivalent of a baby’s smile", "Wait for it...", "Ted! Ted!", "Haveee you met Ted?"};
+
+
+            res.Text = responses[new Random().Next(0, responses.Length)];
+        }
+    }
+}
